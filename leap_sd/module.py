ac6c1f227ddcca67dc018a53ff4e6409975714f4
Peter Willemsen <peter@codebuffet.co>
2023-01-31 12:48:28 +0100

    cleaned up unroll in its own layer

--- leap_sd/module.py
+++ leap_sd/module.py
@@ -8,9 +8,46 @@ from itertools import chain
 from torch import nn, einsum
 import torch.nn.functional as F
 
+class LEAPBuffer(nn.Module):
+    def __init__(self, size_in: int, size_out: int, hidden_size: int, n_hidden: int, dropout_p: float = 0.01):
+        super().__init__()
+        self.size_in = size_in
+        self.size_out = size_out
+        self.hidden_size = hidden_size
+        self.n_hidden = n_hidden
+        self.dropout_p = dropout_p
+        self.init_model()
+
+    def init_model(self):
+        self.net_in = nn.Linear(self.size_in, self.hidden_size)
+        hidden_layers = []
+        for i in range(self.n_hidden):
+            hidden_layers += [
+                nn.Linear(self.hidden_size, self.hidden_size),
+                nn.LeakyReLU(),
+                nn.Dropout(p=self.dropout_p)
+            ]
+        self.net_hidden = nn.Sequential(*hidden_layers)
+            
+    def unroll_buffer(self, x):
+        amount_to_unroll = math.ceil(self.size_out / self.hidden_size)
+        sinspace = torch.sin(torch.linspace(0, 2 * math.pi, self.hidden_size * amount_to_unroll, device=x.device))
+        result = torch.zeros(x.shape[0], self.hidden_size * amount_to_unroll, device=x.device)
+        for idx in range(amount_to_unroll):
+            positional_encoding = sinspace[self.hidden_size * idx:self.hidden_size * (idx + 1)]
+            # positional_encoding += abs(torch.min(positional_encoding))
+            # positional_encoding /= torch.max(positional_encoding)
+            positional_encoding = positional_encoding.expand(x.shape[0], -1)
+            result[:, self.hidden_size * idx:self.hidden_size * (idx + 1)] = self.net_hidden(x + positional_encoding)
+        return result[:, :self.size_out]
+
+    def forward(self, x):
+        x = self.net_in(x)
+        return self.unroll_buffer(x)
+
 class LEAPBlock(nn.Module):
     def __init__(self, act_fn, subsample_count: int, c_out: int, stride: int):
-        super().__init__()
+        super().__init__() 
         self.net = nn.Sequential(
             nn.AvgPool2d(subsample_count),
             nn.Conv2d(
@@ -30,6 +67,8 @@ class LM(pl.LightningModule):
         self,
         steps,
         input_shape,
+        mapping,
+        compress = 8,
         min_weight = 0,
         max_weight = 0,
         learning_rate=1e-4,
@@ -43,6 +82,8 @@ class LM(pl.LightningModule):
     ):
         super().__init__()
         self.save_hyperparameters()
+        self.compress = compress
+        self.mapping = mapping
         self.min_weight = min_weight
         self.max_weight = max_weight
         self.latent_dim_size = latent_dim_size
@@ -63,6 +104,23 @@ class LM(pl.LightningModule):
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
+
+    def _create_output_layer(self, output_size: int, dropout_p: int):
+        output_layers = [
+            LEAPBuffer(128, output_size, 128, 10)
+        ]
+        return nn.Sequential(*output_layers)
+    
+    def init_inn(self, dropout_p):
+        keys = list(self.mapping.keys())
+        keys.sort()
+        for key in keys:
+            inn_name = f"inn_{key}"
+            mapping = self.mapping[key]
+            size = mapping['len']
+            print(f"Setting {inn_name} with len {size}")
+            output_layer = self._create_output_layer(size, dropout_p)
+            setattr(self, inn_name, output_layer)
     
     def init_model(self, input_shape, dropout_p):
         self.leap_block_1 = LEAPBlock(self.resnet_act_fn, 1, 64, 8)
@@ -71,32 +129,8 @@ class LM(pl.LightningModule):
         features_size = self._get_conv_output(input_shape)
         self.features_size = features_size
         print("features_size", features_size)
-        output_layers = [
-        ]
-        for i in range(self.n_latent_dim_layers):
-            if i == 0:
-                output_layers += [
-                    nn.Linear(features_size, self.latent_dim_buffer_size)
-                ]
-            else:
-                output_layers += [
-                    nn.Linear(self.latent_dim_buffer_size, self.latent_dim_buffer_size)
-                ]
-            if i < (self.n_latent_dim_layers - 1):
-                output_layers += [
-                    nn.LeakyReLU(),
-                    nn.Dropout(p=dropout_p)
-                ]
-        self.output = nn.Sequential(*output_layers)
-        self.forget_leveler = nn.Sequential(
-            nn.Linear(features_size, 10),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout_p),
-            nn.Linear(10, 5),
-            nn.LeakyReLU(),
-            nn.Dropout(p=dropout_p),
-            nn.Linear(5, 1),
-        )
+        self.init_inn(dropout_p)
+        self.feature_down = nn.Linear(features_size, 128)
         self.init_leapblocks()
 
     def features(self, input):
@@ -136,29 +170,6 @@ class LM(pl.LightningModule):
             }
         return mapping
 
-    @staticmethod
-    def _zero_pad(x, max_len: int):
-        if len(x) < max_len:
-            to_add = torch.zeros(max_len - len(x), device=x.device, dtype=x.dtype)
-            x = torch.cat((x, to_add))
-        return x
-
-    def _unroll_buffer(self, x):
-        result = None
-        amount_to_unroll = math.ceil(self.latent_dim_size / self.latent_dim_buffer_size)
-        linspace = torch.linspace(0, 2 * math.pi, self.features_size * amount_to_unroll, device=self.device)
-        for idx in range(amount_to_unroll):
-            positional_encoding = torch.sin(linspace[self.features_size * idx:self.features_size * (idx + 1)])
-            positional_encoding += abs(torch.min(positional_encoding))
-            positional_encoding /= torch.max(positional_encoding)
-            positional_encoding = positional_encoding.expand(x.shape[0], -1)
-            chunk = self.output(x + positional_encoding)
-            if result is None: 
-                result = chunk
-            else:
-                result = torch.cat((result, chunk), dim=1)
-        return result[:, :self.latent_dim_size]
-
     # will be used during inference
     def forward(self, x):
         images_len = x.shape[1]
@@ -170,11 +181,19 @@ class LM(pl.LightningModule):
             else:
                 xf += self.features(image_selection)
         xf = xf / images_len
-        xfo = self.forget_leveler(xf)
-        xf[xf < xfo] = 0
-        xf = self._unroll_buffer(xf)
-        xf = self.denormalize_embed(xf)
-        return xf
+        xfd = self.feature_down(xf)
+        keys = list(self.mapping.keys())
+        keys.sort()
+        tensor = None
+        for key in keys:
+            inn_name = f"inn_{key}"
+            inn_model = getattr(self, inn_name)
+            inn_output = inn_model(xfd)
+            if tensor is None:
+                tensor = inn_output
+            else:
+                tensor = torch.cat((tensor, inn_output), 1)
+        return tensor
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0)
