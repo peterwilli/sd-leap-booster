5cd68b7a2047d53fa8093b2490be927ff7d0d8c9
Peter Willemsen <peter@codebuffet.co>
2023-02-06 13:47:15 +0100

    commit beofore testing older leapbuffers

--- training/tools/lora/hyperparameter_optim.py
+++ training/tools/lora/hyperparameter_optim.py
@@ -55,7 +55,6 @@ def objective(trial):
     for concept in test_concepts:
         instance_data_dir = os.path.abspath(os.path.join(file_path, "..", "..", "test_images", concept))
         cli_path = os.path.abspath(os.path.join(file_path, "..", "..", "..", "bin", "leap_lora"))
-        print("cli_path", cli_path)
         shell_command = f"""
         {sys.executable} -u {cli_path} \
             --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base"  \
